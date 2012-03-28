82e751ee57f19f3139991ac6c501dfa7d55b6634
Nguyễn Tuấn Anh <ubolonton@gmail.com>
2012-03-28 23:20:51 +0700

    use bash (and hopefully make it work in OS X)

--- magit.sh
+++ magit.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+# FIX: This is a hack that needs symlinking and whatnot, fix it
 # Dir that the script is stored
 # echo "$( readlink "$( basename "$0" )" )"
 # echo "$( cd "$( dirname "$0" )" && pwd -P )"
+# BASE_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BASE_DIR="$( dirname "$( readlink -f "$0" )")"
 
-# 256 colors mean better-looking theme
+# # 256 colors mean better-looking theme
 env TERM=xterm-256color COGINI_EMACS=$BASE_DIR emacs -nw -q --no-splash -l $BASE_DIR/init.el
 
